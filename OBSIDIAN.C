dd1870f358f5962d8e7e9fdf20e017131f03bb5b
Obsidian Maximus <obsidianmaximus@gmail.com>
2022-10-01 09:53:18 +0530

    Add files via upload
    First ever c program, got a bit help using net and used most of the logic with my past experience with py :)

---
+++ OBSIDIAN.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+
+{
+  clrscr();
+  printf("Obsidian Maximus's first project in C hehe\n");
+  printf("\n");
+
+
+  int c,d,e,f;
+  float a,b,x;
+
+  a =(float)15;
+  b =(float)7;
+  c = a+b;
+  d = a-b;
+  e = a*b;
+  f = pow(a,b);
+  x = a/b;
+
+  printf("Value of a is : %.f\nValue of b is : %.f\n", a,b);
+  printf("Addition of a and b = %d\n", c);
+  printf("Subtraction of b from a = %d\n", d);
+  printf("Multiplication of a with b = %d\n", e);
+  printf("Raising a to the power of b = %d\n", f);
+  printf("Dividing a by b in float = %.4f\n", x);
+
+}
+, x);
+
+}
