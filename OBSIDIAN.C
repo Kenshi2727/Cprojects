13041e6e86dbb3576d95faf7bd270d9b51bff7d7
Obsidian Maximus <obsidianmaximus@gmail.com>
2022-10-01 10:01:00 +0530

    Update OBSIDIAN.C
    first file code.

--- OBSIDIAN.C
+++ OBSIDIAN.C
@@ -1,19 +1,21 @@
+//just learnt this yesterday along with the sum code. rest all is done by me with minimal help from net and mostly rest with trial and hit error.
+
 #include <stdio.h>
 #include <conio.h>
 
 main()
 
 {
-  clrscr();
-  printf("Obsidian Maximus's first project in C hehe\n");
-  printf("\n");
+  clrscr();  //to clear screen when executing program. must always be before first printf.
+  printf("Obsidian Maximus's first project in C hehe\n");  // \n logic taken from py
+  printf("\n");  //extra line space
 
 
   int c,d,e,f;
   float a,b,x;
 
-  a =(float)15;
-  b =(float)7;
+  a =(float)15; //using casting in line 15 and 16.
+  b =(float)7;  
   c = a+b;
   d = a-b;
   e = a*b;
@@ -25,9 +27,7 @@ main()
   printf("Subtraction of b from a = %d\n", d);
   printf("Multiplication of a with b = %d\n", e);
   printf("Raising a to the power of b = %d\n", f);
-  printf("Dividing a by b in float = %.4f\n", x);
+  printf("Dividing a by b in float = %.4f\n", x);  // %.4f denotes taking float number upto 4 decimal places.
 
-}
-, x);
 
 }
