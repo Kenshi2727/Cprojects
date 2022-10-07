9f22d32b5a18b7e98df4786e04b550de6b2784bf
Obsidian Maximus <obsidianmaximus@pm.me>
2022-10-07 15:59:55 +0530

    Add files via upload
    first program to input function from user using scanf.

---
+++ 7thoctober/myprogram.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+
+{ 
+float a,b,c;
+
+printf("Enter the value for a = \n");
+
+scanf("%f", &a);
+
+printf("Enter the value for b = \n");
+
+scanf("%f", &b);
+
+c = a+b;
+
+printf("The sum of a and b is : %f", c);
+
+
+
+}
+
