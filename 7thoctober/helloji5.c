01fba93f45de469277ee2a044b5de7d9908e11bd
Obsidian Maximus <obsidianmaximus@pm.me>
2022-10-07 17:56:22 +0530

    Add files via upload
    S.I. done.

---
+++ 7thoctober/helloji5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+
+{
+	float P, R, T, SI;
+	
+	printf("Calculating the Simple Interest for a sum of loan ;)\n");
+	
+	printf("\n");
+	
+	printf("Enter the Principal Amount : \n");
+	scanf("%f", &P);
+	
+	printf("Enter the Rate of Interest : \n");
+	
+	scanf("%f", &R);
+	
+	printf("Enter the number of years : \n");
+	
+	scanf("%f", &T);
+	
+	SI = P*R*T;
+	
+	printf("The Simple Interest is : %.2f", SI);
+	
+}
